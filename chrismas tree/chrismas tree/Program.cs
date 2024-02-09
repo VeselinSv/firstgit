205a695bbd4cbf9f3bbb6fc94c450d2b3a1d07ee
Geri Stoyanova <sveselin3@gmail.com>
2024-02-09 08:02:45 +0200

    chrismas tree

---
+++ chrismas tree/chrismas tree/Program.cs
@@ -0,0 +1,69 @@
+﻿Console.Write("Enter hight: ");
+int hight = int.Parse(Console.ReadLine());
+Console.Clear();
+for (int i = 0; i < hight - 1; i++)
+{
+    Console.Write(" ");
+}
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("$");
+for (int i = 0; i < hight; i++)
+{
+    for (int j = 0; j < hight - i - 1; j++)
+    {
+        Console.Write(" ");
+    }
+    for (int k = 0; k < 2 * i + 1; k++)
+    {
+        ConsoleColor randomColor = (ConsoleColor)new Random().Next(16);
+        Console.ForegroundColor = randomColor;
+        if (randomColor == ConsoleColor.Black || randomColor == ConsoleColor.DarkGray)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+        }
+        Console.Write("*");
+        Thread.Sleep(50);
+    }
+    Console.WriteLine();
+}
+for (int d = 0; d < hight - 2; d++)
+{
+    Console.Write(" ");
+}
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("| |");
+while (true)
+{
+    Console.Clear();
+    for (int i = 0; i < hight - 1; i++)
+    {
+        Console.Write(" ");
+    }
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine("$");
+    for (int i = 0; i < hight; i++)
+    {
+        for (int j = 0; j < hight - i - 1; j++)
+        {
+            Console.Write(" ");
+        }
+        for (int k = 0; k < 2 * i + 1; k++)
+        {
+            ConsoleColor randomColor = (ConsoleColor)new Random().Next(16);
+            Console.ForegroundColor = randomColor;
+            if (randomColor == ConsoleColor.Black || randomColor == ConsoleColor.DarkGray)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+            }
+            Console.Write("*");
+        }
+        Console.WriteLine();
+    }
+    for (int d = 0; d < hight - 2; d++)
+    {
+        Console.Write(" ");
+    }
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine("| |");
+    Thread.Sleep(50);
+}
